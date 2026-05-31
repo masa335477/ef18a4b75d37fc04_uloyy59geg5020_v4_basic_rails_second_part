5046183e929efca7b613f10f24bdbe49137e7a52
masa335477 <fujisakim01@gmail.com>
2026-05-31 16:22:48 +0900

    パスワードリセット機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_18_124858) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_24_105050) do
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -49,7 +49,10 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_18_124858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "boards", "users"
